616571ad0fc66eb6e34e4fb26e7e7f944cb9e155
Breno Guimaraes <brenoaraujo@outlook.com.br>
2021-09-26 21:42:16 -0300

    feat: add functions to update the screen in real time along with socket events emmiting

--- public/chat.js
+++ public/chat.js
@@ -3,3 +3,56 @@ const socket = io();
 const urlSeach = new URLSearchParams(window.location.search);
 const username = urlSeach.get('username');
 const room = urlSeach.get('select_room');
+
+document.getElementById('username').innerHTML = `Olá ${username} você está na sala de ${room}`
+
+socket.emit('select_room', {
+  username, 
+  room
+}, messages => {
+  messages.forEach(message => {
+    createMessage(message)
+  })
+});
+
+document
+  .getElementById('message_input')
+  .addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      const message = event.target.value;
+
+      const data = {
+        room,
+        message,
+        username
+      };
+
+      socket.emit('message', data);
+
+      event.target.value = '';
+    }
+})
+
+socket.on('message', data => {
+  createMessage(data);
+})
+
+const createMessage = (data) => {
+  const messageDiv = document.getElementById('messages');
+  messageDiv.innerHTML =`
+    <div class="new_message">
+      <label class="form-label">
+        <strong>${data.username}:</strong>
+        <span>
+          ${data.text} - ${dayjs(data.createdAt).format('DD/MM HH:mm')}
+        </span>
+      </label>
+    </div>
+    `
+}
+
+document
+  .getElementById('logout')
+  .addEventListener('click', (event) => {
+    window.location.href = "index.html"
+  })
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,63 @@
 import { io } from './http';
 
+interface RoomUser {
+  socket_id: string;
+  username: string;
+  room: string;
+}
+
+interface Message {
+  room: string;
+  text: string;
+  username: string;
+  createdAt: Date;
+}
+
+const users: RoomUser[] = []
+const messages: Message[] = []
+
 io.on('connection', socket => {
-  console.log(socket.id);
+
+  socket.on('select_room', (data, callback) => {
+
+    socket.join(data.room);
+
+    const userInRoom = users.find(
+      user => 
+        data.username === user.username && 
+        data.room === user.room
+    )
+
+    if (userInRoom) {
+      userInRoom.socket_id = socket.id
+    } else { 
+      users.push({
+        room: data.room,
+        username: data.username,
+        socket_id: socket.id
+      })
+    }
+
+    const messagesFromRoom = getMessagesRoom(data.room);
+    callback(messagesFromRoom);
+  });
+
+  socket.on('message', data => {
+    const message: Message = {
+      room: data.room,
+      username: data.username,
+      text: data.message,
+      createdAt: new Date()
+    };
+
+    messages.push(message);
+
+    io.to(data.room).emit('message', message)
+  })
 });
+
+const getMessagesRoom = (room: string) => {
+  const messagesFromRoom = messages.filter(message => message.room === room);
+
+  return messagesFromRoom;
+}
